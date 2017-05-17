49758058c3ca3746f980389dd0d6207c535553c8
sudeep <lumbinibhw@gmail.com>
2017-05-17 12:23:46 +0545

    banner add class

--- inc/so-widgets/banner/banner.php
+++ inc/so-widgets/banner/banner.php
@@ -1,200 +1,90 @@
-<?php
-
-/*
-
-Widget Name: Banner widget
-
-Description: A very simple banner widget.
-
-Author: jThemes Studio
-
-Author URI: http://jthemesstudio.com
-
-*/
-
-
-
-class SiteOrigin_Widget_Banner_Widget extends SiteOrigin_Widget {
-
-	function __construct() {
-
-		parent::__construct(
-
-			'sow-banner',
-
-			__('SiteOrigin Banner', 'siteorigin-widgets'),
-
-			array(
-
-				'description' => __('A responsive banner widget that supports images and video.', 'siteorigin-widgets'),
-
-				'help' => 'http://siteorigin.com/widgets-bundle/banner-widget-documentation/',
-
-				'panels_title' => false,
-
-			),
-
-			array(
-
-
-
-			),
-
-			array(			
-
-						
-
-						'background_image' => array(
-
-							'type' => 'media',
-
-							'library' => 'image',
-
-							'label' => __('Background image', 'siteorigin-widgets'),
-
-						),
-
-
-
-
-
-						'align' => array(
-
-							'type' => 'select',
-
-							'label' => __('Text alignment', 'siteorigin-widgets'),
-
-							'options' => array(
-
-								'left' => __('Left', 'siteorigin-widgets'),
-
-								'right' => __('Right', 'siteorigin-widgets'),
-
-								'center' => __('Center', 'siteorigin-widgets'),								
-
-							)
-
-						),
-
-						'style' => array(
-
-							'type' => 'select',
-
-							'label' => __('Text Style', 'siteorigin-widgets'),
-
-							'options' => array(
-
-								'normal' => __('Normal Style', 'siteorigin-widgets'),
-
-								'alt' => __('Alternate Style', 'siteorigin-widgets'),
-
-								'big-text' => __('Big Text Style', 'siteorigin-widgets'),								
-
-							)
-
-						),
-
-		
-
-				
-
-						'title' => array(
-
-							'type' => 'textarea',
-
-							'label' => __('Banner title', 'siteorigin-widgets'),
-
-						),
-
-
-
-
-
-						'subtitle' => array(
-
-							'type' => 'textarea',
-
-							'label' => __('Layer subtitle', 'siteorigin-widgets'),
-
-						),
-
-						
-
-						'btntext' => array(
-
-							'type' => 'text',
-
-							'label' => __('More Button Text', 'siteorigin-widgets'),
-
-							
-
-						),
-
-						'btnurl' => array(
-
-							'type' => 'text',
-
-							'label' => __('More Button URL', 'siteorigin-widgets'),
-
-						
-
-						),
-
-						'new_window' => array(
-
-						'type' => 'checkbox',
-
-						'label' => __('Open Button URL in New Window', 'siteorigin-widgets'),
-
-						'default' => false,
-
-						),
-
-
-
-						
-
-					
-
-				
-
-
-
-			),
-
-			plugin_dir_path(__FILE__).'../'
-
-		);
-
-	}
-
-
-
-
-
-	function get_style_name($instance){
-
-		return false;
-
-	}
-
-
-
-	function get_template_name($instance){
-
-		return 'base';
-
-	}
-
-
-
-	
-
-
-
-}
-
-
-
-siteorigin_widget_register('banner', __FILE__);
-
+<?php
+/*
+Widget Name: Banner widget
+Description: A very simple banner widget.
+Author: jThemes Studio
+Author URI: http://jthemesstudio.com
+*/
+class SiteOrigin_Widget_Banner_Widget extends SiteOrigin_Widget {
+	function __construct() {
+		parent::__construct(
+			'sow-banner',
+			__('SiteOrigin Banner', 'siteorigin-widgets'),
+			array(
+				'description' => __('A responsive banner widget that supports images and video.', 'siteorigin-widgets'),
+				'help' => 'http://siteorigin.com/widgets-bundle/banner-widget-documentation/',
+				'panels_title' => false,
+			),
+			array(
+			),
+			array(			
+						
+						'background_image' => array(
+							'type' => 'media',
+							'library' => 'image',
+							'label' => __('Background image', 'siteorigin-widgets'),
+						),
+						'align' => array(
+							'type' => 'select',
+							'label' => __('Text alignment', 'siteorigin-widgets'),
+							'options' => array(
+								'left' => __('Left', 'siteorigin-widgets'),
+								'right' => __('Right', 'siteorigin-widgets'),
+								'center' => __('Center', 'siteorigin-widgets'),								
+							)
+						),
+						'style' => array(
+							'type' => 'select',
+							'label' => __('Text Style', 'siteorigin-widgets'),
+							'options' => array(
+								'normal' => __('Normal Style', 'siteorigin-widgets'),
+								'alt' => __('Alternate Style', 'siteorigin-widgets'),
+								'big-text' => __('Big Text Style', 'siteorigin-widgets'),								
+							)
+						),
+		
+				
+						'title' => array(
+							'type' => 'textarea',
+							'label' => __('Banner title', 'siteorigin-widgets'),
+						),
+						'subtitle' => array(
+							'type' => 'textarea',
+							'label' => __('Layer subtitle', 'siteorigin-widgets'),
+						),
+						
+						'btntext' => array(
+							'type' => 'text',
+							'label' => __('More Button Text', 'siteorigin-widgets'),
+							
+						),
+						'btnurl' => array(
+							'type' => 'text',
+							'label' => __('More Button URL', 'siteorigin-widgets'),
+						
+						),
+						'new_window' => array(
+						'type' => 'checkbox',
+						'label' => __('Open Button URL in New Window', 'siteorigin-widgets'),
+						'default' => false,
+						),
+						'extra_class' => array(
+						'type' => 'text',
+						'label' => __('Extra class fro html', 'siteorigin-widgets'),
+						),
+						
+					
+				
+			),
+			plugin_dir_path(__FILE__).'../'
+		);
+	}
+	function get_style_name($instance){
+		return false;
+	}
+	function get_template_name($instance){
+		return 'base';
+	}
+	
+}
+siteorigin_widget_register('banner', __FILE__);
--- inc/so-widgets/banner/tpl/base.php
+++ inc/so-widgets/banner/tpl/base.php
@@ -1,33 +1,66 @@
-<?php if($instance['style']=='alt')
-    $style='alt-font';
-    elseif($instance['style']=='big-text')
-    $style='alt-font big-text';
-    else
-    $style='';
-?>
-<div class="thumbnail no-border no-padding thumbnail-banner <?php echo $style;?>">
-    <div class="media">
-        <?php  $image=wp_get_attachment_image_src($instance['background_image'], 'full'); ?>
-        <?php if( !empty( $instance['btnurl'] ) ) echo '<a  href="' . esc_url( $instance['btnurl'] ) . '" ' . ( $instance['new_window'] ? 'target="_blank"' : '' ) . ' class="media-link" >'; ?>
-            <img src="<?php echo $image[0]; ?>" alt=""/>
-            <div class="caption text-<?php echo $instance['align'] ?>">
-                <div class="caption-wrapper div-table">
-                <div class="caption-inner div-cell">
-                    <h2 class="caption-title"><span><?php echo esc_attr($instance['title']); ?></span></h2>
-                    <h3 class="caption-sub-title"><span><?php echo esc_attr($instance['subtitle']); ?></span></h3>
-                    <?php if( !empty( $instance['btnurl'] ) ) : ?>
-                    <span class="btn btn-theme btn-theme-sm"><?php echo wp_kses_post( $instance['btntext'] ) ?></span>
-                    <?php endif; ?>
-                </div>
-                </div>
-            </div>
-         <?php if( !empty( $instance['btnurl'] ) ) echo '</a>'; ?>
-    </div>
-</div>
-
-	
-
-
-
-
-
+<?php if($instance['style']=='alt')
+
+    $style='alt-font';
+
+    elseif($instance['style']=='big-text')
+
+    $style='alt-font big-text';
+
+    else
+
+    $style='';
+
+?>
+
+<div class="thumbnail no-border no-padding thumbnail-banner <?php echo $instance['extra_class']?> <?php echo $style;?>">
+
+    <div class="media">
+
+        <?php  $image=wp_get_attachment_image_src($instance['background_image'], 'full'); ?>
+
+        <?php if( !empty( $instance['btnurl'] ) ) echo '<a  href="' . esc_url( $instance['btnurl'] ) . '" ' . ( $instance['new_window'] ? 'target="_blank"' : '' ) . ' class="media-link" >'; ?>
+
+            <img src="<?php echo $image[0]; ?>" alt=""/>
+
+            <div class="caption text-<?php echo $instance['align'] ?>">
+
+                <div class="caption-wrapper div-table">
+
+                <div class="caption-inner div-cell">
+
+                    <h2 class="caption-title"><span><?php echo esc_attr($instance['title']); ?></span></h2>
+
+                    <h3 class="caption-sub-title"><span><?php echo esc_attr($instance['subtitle']); ?></span></h3>
+
+                    <?php if( !empty( $instance['btnurl'] ) ) : ?>
+
+                    <span class="btn btn-theme btn-theme-sm"><?php echo wp_kses_post( $instance['btntext'] ) ?></span>
+
+                    <?php endif; ?>
+
+                </div>
+
+                </div>
+
+            </div>
+
+         <?php if( !empty( $instance['btnurl'] ) ) echo '</a>'; ?>
+
+    </div>
+
+</div>
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
